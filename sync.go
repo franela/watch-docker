14bdbc667c41f8d6237a075401ca3ac1779a9463
Jonathan Leibiusky (@xetorthio) <ionathan@gmail.com>
2017-04-19 15:44:56 -0500

    Add sync process

---
+++ sync.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/google/go-github/github"
+
+	mgo "gopkg.in/mgo.v2"
+
+	"golang.org/x/oauth2"
+)
+
+type Pull struct {
+	Number int64 `json:number`
+}
+
+func main() {
+	token := ""
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("github").C("pulls")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	lastPull := Pull{}
+	err = c.Find(nil).Sort("-number").One(&lastPull)
+	if err != nil {
+		log.Fatal("Could not get the last pull request from DB", err)
+	}
+
+	synced := false
+	for !synced {
+		opts := &github.PullRequestListOptions{State: "closed"}
+		prs, resp, err := client.PullRequests.List(ctx, "moby", "moby", opts)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		for _, pr := range prs {
+			if *pr.Number <= int(lastPull.Number) {
+				synced = true
+				break
+			}
+			if pr.MergedAt == nil {
+				continue
+			}
+			pr, _, err := client.PullRequests.Get(ctx, "moby", "moby", *pr.Number)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			err = c.Insert(pr)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Inserted PR [%d]\n", *pr.Number)
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.ListOptions.Page = resp.NextPage
+	}
+}
